6e68d02e157b381a7561a7dfd111d300e748726d
sebastian <sebastian@boolean.cl>
2023-02-02 16:11:49 -0300

    solved ready

---
+++ array_functions/01_map.js
@@ -0,0 +1,41 @@
+/*
+  1) Función map
+  Crea una función llamada map que reciba un arreglo y un callback.
+  Por cada elemento del arreglo la función ejecuta el callback pasando 3 parámetros:
+  elemento, indice y el arreglo original.
+
+  La función map debe devolver un nuevo arreglo con los valores devueltos por el callback.
+
+  Ejemplos de uso:
+
+  a) Pasando todos los parámetros:
+
+  map(['a','b','c'], function(item,index,array){
+    return(item + " " + index + " " + array)
+  })
+
+  => Array(3) [ "a 0 a,b,c", "b 1 a,b,c", "c 2 a,b,c" ]
+
+  b) Pasando solo el primer parámetro:
+
+  map(['a','b','c'], function(item){
+    return(item.toUpperCase())
+  })
+
+  => Array(3) [ "A", "B", "C" ]
+*/
+
+function map(array, callback){
+  var arrayLength = array.length;
+	var newArray = [];
+
+	for (var i = 0; i < arrayLength; i++) {
+    // Tu código acá:
+    newArray.push(callback(array[i], i, array))
+  }
+  return newArray;
+}
+
+module.exports = {
+  map: map
+}
